46379bfd89a55ba5a073b6694cc35e6cae3103f8
Maciej Nycz <nycz.maciej91@gmail.com>
2018-12-14 09:13:18 +0000

    better version

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://limitless-lake-28436.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  const trelloApiRoot = 'https://limitless-lake-28436.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
